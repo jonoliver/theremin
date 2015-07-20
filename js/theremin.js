2a02aff0df3d9a0d962725fe9377339466db837e
Jon Oliver <jon@joliver.net>
2015-07-19 23:56:40 -0400

    Add vibrato

--- js/theremin.js
+++ js/theremin.js
@@ -21,3 +21,29 @@ function updateTone(e) {
   oscillator.frequency.value = posX
   gainNode.gain.value = posY  
 }
+
+var vibratoRate = 50;
+var vibratoInterval = setInterval(vibrato, vibratoRate);
+
+var detuneVal = 10;
+var detuneMultiplier = 10
+
+function vibrato() {
+  detuneMultiplier = detuneMultiplier * -1;
+  oscillator.detune.value = detuneVal * detuneMultiplier;
+}
+
+document.onkeydown = handleKey;
+
+function handleKey(e) {
+  switch(e.keyIdentifier){
+    case "Down":
+      if (detuneVal > 0) detuneVal--;
+      console.log(detuneVal);
+      break;
+    case "Up":
+      detuneVal++;
+      console.log(detuneVal);
+      break;
+  }
+}
